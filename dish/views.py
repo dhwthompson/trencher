47067ddf962f07e4609cf8e4b848014cd82cd2c4
David Thompson <mail@fatbusinessman.com>
2020-04-01 14:50:32 -0700

    Make it possible (and necessary) to log in
    Might as well add a spot of security before adding the ability to modify
    anything (outside the admin, of course).

--- dish/views.py
+++ dish/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from .models import Dish
 
 
+@login_required
 def index(request):
     context = {"dishes": Dish.objects.order_by("name")}
     return render(request, "dish/index.html", context)
--- trencher/urls.py
+++ trencher/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 
 from dish import views as dish_views
 
 urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path('', dish_views.index),
 ]
