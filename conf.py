cb1d8ffb92bd7d939a9a550f58e4f2667b19879b
David Thompson <mail@fatbusinessman.com>
2020-05-12 17:36:58 -0700

    Add Heroku metadata into traces
    This is the easiest way I could find to narrow traces down to specific
    app versions.

--- conf.py
+++ conf.py
@@ -7,4 +7,10 @@ import beeline
 def post_worker_init(worker):
     logging.info(f'beeline initialization in process pid {os.getpid()}')
     api_key = os.environ.get('HONEYCOMB_API_KEY')
-    beeline.init(writekey=api_key, dataset="trencher", debug=True)
+    beeline.init(writekey=api_key, dataset="trencher", debug=True, presend_hook=presend)
+
+
+def presend(fields):
+    for key, value in os.environ.items():
+        if key.startswith('HEROKU_'):
+            fields[key.lower()] = value
