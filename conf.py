e72703da233fd93328cec49be8b9b3834911d4c3
David Thompson <mail@fatbusinessman.com>
2020-05-21 14:58:38 -0700

    Get Honeycomb dataset name from the environment
    This allows for a separate dev dataset, to test things out before
    deploying them.

--- conf.py
+++ conf.py
@@ -7,7 +7,8 @@ import beeline
 def post_worker_init(worker):
     logging.info(f'beeline initialization in process pid {os.getpid()}')
     api_key = os.environ.get('HONEYCOMB_API_KEY')
-    beeline.init(writekey=api_key, dataset="trencher", debug=True, presend_hook=presend)
+    dataset = os.environ.get('HONEYCOMB_DATASET')
+    beeline.init(writekey=api_key, dataset=dataset, debug=True, presend_hook=presend)
 
 
 def presend(fields):
