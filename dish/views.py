ddaf15d2805148f5e2cfe614d4596bb3598a9f44
David Thompson <mail@fatbusinessman.com>
2020-05-21 15:43:45 -0700

    Remove the views from the dish app
    For the moment, we can manage these through the admin. The `shop` view
    from the `plan` app lets us browse through a list of all available
    dishes.

--- dish/views.py
+++ dish/views.py
@@ -3,29 +3,4 @@ from django.forms import ModelForm
 from django.shortcuts import redirect, render
 from django.views.decorators.http import require_http_methods
 
-from .models import Dish
-
-
-class NewDishForm(ModelForm):
-    class Meta:
-        model = Dish
-        fields = ["name"]
-
-    def __init__(self, *args, **kwargs):
-        super(ModelForm, self).__init__(*args, **kwargs)
-        self["name"].field.widget.attrs["required"] = "required"
-
-
-@require_http_methods(['GET', 'POST'])
-@login_required
-def index(request):
-    if request.method == "POST":
-        form = NewDishForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('index')
-    else:
-        form = NewDishForm()
-
-    context = {"dishes": Dish.objects.order_by("name"), "form": form}
-    return render(request, "dish/index.html", context)
+# This app doesn't have any views of its own
