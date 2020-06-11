f18c10a913e61ec78e86ebbfd3ba4ea8ad38cce0
David Thompson <mail@fatbusinessman.com>
2020-06-11 15:08:08 -0700

    Configure SSL-forcing from the environment
    I *really* don't want to faff around with SSL on the development server.
    
    This defaults to forcing SSL if nothing is configured (or with anything
    other than a specific "false" value) so that this fails into the more
    secure direction.

--- trencher/settings.py
+++ trencher/settings.py
@@ -132,7 +132,7 @@ USE_L10N = True
 USE_TZ = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = os.environ.get("FORCE_SSL") != "false"
 
 
 # Static files (CSS, JavaScript, Images)
