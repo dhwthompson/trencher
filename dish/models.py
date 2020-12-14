679ebe53537edd87cac2a8b09f24893cf92152cf
David Thompson <mail@fatbusinessman.com>
2020-12-14 15:45:08 -0800

    Add a field for where we store ingredients
    This should help streamline the order creation process a bit, and save
    us having to look in the fridge a whole bunch of times as we progress
    through the alphabet.

--- dish/models.py
+++ dish/models.py
@@ -1,6 +1,7 @@
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
+
 class DishManager(models.Manager):
     def active(self):
         return self.filter(deactivated=False)
@@ -32,6 +33,25 @@ class IngredientSection(models.TextChoices):
     VEG = "veg", "Fruit and veg"
 
 
+class IngredientStorage(models.TextChoices):
+    """Identify different storage areas for ingredients.
+
+    This model functions very similarly to the `IngredientSection` model above, except
+    that it refers to where we store ingredients once we've bought them, rather than
+    where in the store we can find them. As such, it's useful during the ingredient
+    sift, rather than during the shop itself.
+
+    There's probably an argument here for combining these two models into one, but
+    that's merging two separate concepts into one because they happen to be the same
+    shape. Maybe if we were to add a third classification of ingredients, we could start
+    looking at a more general-purpose classification model.
+    """
+
+    FREEZER = "freezer", "Freezer"
+    FRIDGE = "fridge", "Fridge"
+    LARDER = "larder", "Larder"
+
+
 class Ingredient(models.Model):
     class Meta:
         ordering = ["name"]
@@ -40,6 +60,9 @@ class Ingredient(models.Model):
     dishes = models.ManyToManyField(Dish, related_name="ingredients")
     section = models.CharField(
         max_length=10, blank=True, choices=IngredientSection.choices
+    )
+    storage = models.CharField(
+        max_length=10, blank=True, choices=IngredientStorage.choices
     )
 
     def __str__(self):
